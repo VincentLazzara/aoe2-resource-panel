0c2272d613cdf9874415a476af693f94135a574e
Vincent Lazzara <vincent.a.lazzara@gmail.com>
2024-12-07 22:18:53 -0500

    Initial Commit

---
+++ aoe2-synthetic-data/aoe2_synthetic_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  aoe2_synthetic_dataApp.swift
+//  aoe2-synthetic-data
+//
+//  Created by Vincent Lazzara on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct aoe2_synthetic_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
